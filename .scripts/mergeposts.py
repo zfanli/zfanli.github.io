8b9c8e1101ee91a14e3d4a853805ad2e4ff95bad
Richard <zfanli@outlook.com>
2021-08-08 22:47:50 +0800

    Update merge script

--- .scripts/mergeposts.py
+++ .scripts/mergeposts.py
@@ -1,13 +1,42 @@
 from pathlib import Path
 import argparse
+import shutil
+import re
 
 SOURCE = Path(".").parent.joinpath("source")
 POSTS_DIR = SOURCE.joinpath("_posts")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
+    # in path: posts need to be merged
     parser.add_argument("in_path", metavar="i", type=str)
+    # out path: the path posts need to be merged into
     parser.add_argument("out_path", metavar="o", type=str)
-
+    # get arguments
     args = parser.parse_args()
-    print(args.in_path, args.out_path)
+    # print(args.in_path, args.out_path)
+
+    output = POSTS_DIR.joinpath(args.out_path)
+    # remove output folder if exists
+    if output.exists():
+        shutil.rmtree(output)
+    output.mkdir()
+
+    for child in Path(args.in_path).iterdir():
+        if child.is_dir():
+            shutil.copytree(child, output.joinpath(child.name))
+        else:
+            # make the output name
+            name = ""
+            for n in child.name:
+                # keep letters and numbers only, replace others as hyphens
+                if re.match(r"[a-zA-Z0-9]", n):
+                    name += n
+                else:
+                    name += "-"
+            # remove contiguous hyphens
+            name = re.sub(r"--+", "-", name)
+            # remove trailing hyphen
+            name = re.sub(r"-$", "", name)
+
+            shutil.copy2(child, output.joinpath(name))
